d2373953b2df88fbd06c67a96f59a30774669ba5
Hoang <miinhhoangg204@gmail.com>
2024-08-25 22:26:17 +0700

    first commit

---
+++ app/baitap-ss09/bai7/Bai7.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AddProjectForm from '@/components/AddProjectForm'
+export default function Bai7() {
+  return (
+    <div className="p-4">
+       <AddProjectForm />
+    </div>
+  )
+}
